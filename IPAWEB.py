cf215b46a3161421d889e0c06da755d95d3e95ed
ivanchi88 <ivanmotos_8@hotmail.es>
2016-04-17 01:42:03 +0200

    Changed the grid distribution

--- IPAWEB.py
+++ IPAWEB.py
@@ -30,108 +30,79 @@ class VentanaNav(QtGui.QWidget):
 
 	#Pantalla Principal
 	def crearPantallaPrincipal(self):
-		self.gridLayout = QtGui.QGridLayout(self)
+		#Crear la tabla principal
+		self.mainLayout = QtGui.QGridLayout(self)
+		self.crearTablas()
 
-		self.btnAtras = QtGui.QPushButton(self, text = 'Volver')
-		self.gridLayout.addWidget(self.btnAtras, 1, 0, 1, 1)
-
-		self.btnAdelante = QtGui.QPushButton(self, text = 'Adelante')
-		self.gridLayout.addWidget(self.btnAdelante, 1, 1, 1, 1)
-
-		self.direcc = QtGui.QLabel(u'Dirección', self)
-		self.gridLayout.addWidget(self.direcc, 1, 2, 1, 1)
-
-		self.urlBox = QtGui.QLineEdit(self)
-		self.gridLayout.addWidget(self.urlBox, 1, 3, 1, 1)
-
-		'''
-		self.btnMenu = QtGui.QPushButton(self, text = 'Menu')
-		self.gridLayout.addWidget(self.btnMenu, 0, 0, 1, 1)
-		QtGui.QWidget.hide(self.btnMenu)
-		'''
-		self.navegadorP = QtWebKit.QWebView(self)
-		self.gridLayout.addWidget(self.navegadorP, 2, 0, 1, 4)
-
-		#Url de inicio
-		_url = "http://www.google.es"
-		self.navegadorP.load(QtCore.QUrl(_url))
 		#cosas raras
 		self.webkitThings()
 		#manejadores de eventos
 		self.acciones()
 
+	def crearTablas(self):
+		#Crear las tablas secundarias
+		self.gridLayout1 = QtGui.QGridLayout(self)
+		self.gridLayout2 = QtGui.QGridLayout(self)
+		self.gridLayout3 = QtGui.QGridLayout(self)
+
+		#Añadir las tablas secundarias a la principal
+		self.mainLayout.addLayout(self.gridLayout1, 0, 0)
+		self.mainLayout.addLayout(self.gridLayout2, 1, 0,)
+		self.mainLayout.addLayout(self.gridLayout3, 2, 0)
+		#Rellenar las tablas
+		self.tabla1()
+		self.tabla2()
+		self.tabla3()
+
+	def tabla1(self):
+		self.pestanyasLayout = QtGui.QGridLayout(self)
+		self.gridLayout1.addLayout(self.pestanyasLayout, 0, 0, 0, 20)
+
+		self.masPestanyas = QtGui.QPushButton(self, text = 'MAS')
+		self.gridLayout1.addWidget(self.masPestanyas, 0, 19, 1, 1)
+
+		self.btnMenu = QtGui.QPushButton(self, text = 'Menu')
+		self.gridLayout1.addWidget(self.btnMenu, 0, 20, 1, 1)
+		#QtGui.QWidget.hide(self.btnMenu)
+
+	def tabla2(self):
+		self.btnAtras = QtGui.QPushButton(self, text = 'Volver')
+		self.gridLayout2.addWidget(self.btnAtras, 0, 0, 1, 1)
+
+		self.btnAdelante = QtGui.QPushButton(self, text = 'Adelante')
+		self.gridLayout2.addWidget(self.btnAdelante, 0, 1, 1, 1)
+
+		self.direcc = QtGui.QLabel(u'Dirección', self)
+		self.gridLayout2.addWidget(self.direcc, 0, 2, 1, 1)
+
+		self.urlBox = QtGui.QLineEdit(self)
+		self.gridLayout2.addWidget(self.urlBox, 0, 3, 1, 1)
+
+	def tabla3(self):
+		#El navegador en sí
+		self.navegadorP = QtWebKit.QWebView(self)
+		self.gridLayout3.addWidget(self.navegadorP, 0, 0, 1, 1)
+		#Url de inicio
+		_url = "http://www.google.es"
+		self.urlBox.setText(str(_url))
+		self.navegadorP.load(QtCore.QUrl(_url))
+		
 	'''
-	#Segunda Pantalla Vertical
-	def SegundaPantallaVer(self):
-
-		self.btnAtrasV = QtGui.QPushButton(self, text = 'Volver')
-		self.gridLayout.addWidget(self.btnAtrasV, 1, 4, 1, 1)
-
-		self.btnAdelanteV = QtGui.QPushButton(self, text = 'Adelante')
-		self.gridLayout.addWidget(self.btnAdelanteV, 1, 5, 1, 1)
-
-		self.direccV = QtGui.QLabel(u'Dirección', self)
-		self.gridLayout.addWidget(self.direccV, 1, 6, 1, 1)
-
-		self.urlBoxV = QtGui.QLineEdit(self)
-		self.gridLayout.addWidget(self.urlBoxV, 1, 8, 1, 1)
-
 		self.navegadorV = QtWebKit.QWebView(self)
 		self.gridLayout.addWidget(self.navegadorV, 2, 4, 1, 5)
 
 		_url = "http://www.google.es"
 		self.navegadorV.load(QtCore.QUrl(_url))
 
-	#Ocultar la segunda pantalla vertical
-	def ocultarSecPantVer(self):
-		QtGui.QWidget.hide(self.btnAtrasV)
-		QtGui.QWidget.hide(self.btnAdelanteV)
-		QtGui.QWidget.hide(self.direccV)
-		QtGui.QWidget.hide(self.urlBoxV)
-		QtGui.QWidget.hide(self.navegadorV)
-
-	def mostrarSecPantVer(self):
-		QtGui.QWidget.show(self.btnAtrasV)
-		QtGui.QWidget.show(self.btnAdelanteV)
-		QtGui.QWidget.show(self.direccV)
-		QtGui.QWidget.show(self.urlBoxV)
-		QtGui.QWidget.show(self.navegadorV)
-
-	#Segunda Pantalla Horizontal
-	def SegundaPantallaHor(self):
-		self.btnAtrasH = QtGui.QPushButton(self, text = 'Volver')
-		self.gridLayout.addWidget(self.btnAtrasH, 3, 0, 1, 1)
-
-		self.btnAdelanteH = QtGui.QPushButton(self, text = 'Adelante')
-		self.gridLayout.addWidget(self.btnAdelanteH, 3, 1, 1, 1)
-
-		self.direccH = QtGui.QLabel(u'Dirección', self)
-		self.gridLayout.addWidget(self.direccH, 3, 2, 1, 1)
-
-		self.urlBoxH = QtGui.QLineEdit(self)
-		self.gridLayout.addWidget(self.urlBoxH, 3, 3, 1, 6)
-
 		#Se crea una 'segunda pantalla' que por defecto está oculta. *Tengo que implementar un menu con la opcion de mostrarla.
 		self.navegadorH = QtWebKit.QWebView(self)
 		self.gridLayout.addWidget(self.navegadorH, 4, 0, 1, 9)
-		# QtGui.QWidget.hide(self.navegador2) # esto oculto navegador2
-		_url = "http://www.google.es"
-		self.navegadorH.load(QtCore.QUrl(_url))
-	#De momento lo voy a ocultar, porque no se que sera mas eficiente, si ir creando y destruyendo o tan solo ocultando.
-	def ocultarSecPantHor(self):
-		QtGui.QWidget.hide(self.btnAtrasH)
-		QtGui.QWidget.hide(self.btnAdelanteH)
-		QtGui.QWidget.hide(self.direccH)
-		QtGui.QWidget.hide(self.urlBoxH)
-		QtGui.QWidget.hide(self.navegadorH)
 
-	def mostrarSecPantHor(self):
-		QtGui.QWidget.show(self.btnAtrasH)
-		QtGui.QWidget.show(self.btnAdelanteH)
-		QtGui.QWidget.show(self.direccH)
-		QtGui.QWidget.show(self.urlBoxH)
-		QtGui.QWidget.show(self.navegadorH)
+		# QtGui.QWidget.hide(self.navegador2) # esto oculta navegadorH
+
+	#De momento lo voy a ocultar, porque no se que sera mas eficiente, si ir creando y destruyendo o tan solo ocultando.
 	'''
+
 	def webkitThings(self): #Ni idea de que es esto, pero parece para activar funcionalidades extra. Investigar en un futuro.
 		QtNetwork.QNetworkProxyFactory.setUseSystemConfiguration(True)
 		QtWebKit.QWebSettings.globalSettings().setAttribute(
@@ -184,5 +155,5 @@ def main():
 	VentanaNavegadorP.show()
 	sys.exit(app.exec_())
 	
-if __name__ == main():
+if __name__ == '__main__':
 	main()
