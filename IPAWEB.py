b00238ef85768846fb0606d6850fa48886714e40
ivanchi88 <ivanmotos_8@hotmail.es>
2016-04-18 02:37:03 +0200

    Update IPAWEB.py

--- IPAWEB.py
+++ IPAWEB.py
@@ -27,13 +27,13 @@ class VentanaNav(QtGui.QWidget):
 		self.showMaximized()
 		self.setWindowTitle("IPAWEB") # Nombre del programa
 		self.setWindowIcon(QtGui.QIcon('main_icon.ico')) # Icono del programa
+		# self.setWindowFlags(QtCore.Qt.FramelessWindowHint) #Tengo que mejorarlo aun
 
 	#Pantalla Principal
 	def crearPantallaPrincipal(self):
 		#Crear la tabla principal
 		self.mainLayout = QtGui.QGridLayout(self)
 		self.crearTablas()
-
 		#cosas raras
 		self.webkitThings()
 		#manejadores de eventos
@@ -59,24 +59,45 @@ class VentanaNav(QtGui.QWidget):
 		self.gridLayout1.addLayout(self.pestanyasLayout, 0, 0, 0, 20)
 
 		self.masPestanyas = QtGui.QPushButton(self, text = 'MAS')
-		self.gridLayout1.addWidget(self.masPestanyas, 0, 19, 1, 1)
+		self.gridLayout1.addWidget(self.masPestanyas, 0, 16, 1, 1)
 
 		self.btnMenu = QtGui.QPushButton(self, text = 'Menu')
-		self.gridLayout1.addWidget(self.btnMenu, 0, 20, 1, 1)
+		self.gridLayout1.addWidget(self.btnMenu, 0, 17, 1, 1)
+
+		self.btnMin = QtGui.QPushButton(self)
+		self.btnMin.setIcon(QtGui.QIcon('min.ico'))
+		self.gridLayout1.addWidget(self.btnMin, 0, 18, 1, 1 )
+
+		self.btnMax = QtGui.QPushButton(self)
+		self.btnMax.setIcon(QtGui.QIcon('max.ico'))
+		self.gridLayout1.addWidget(self.btnMax, 0, 19, 1, 1 )
+
+		self.btnQuitar = QtGui.QPushButton(self)
+		self.btnQuitar.setIcon(QtGui.QIcon('close.ico'))
+		self.gridLayout1.addWidget(self.btnQuitar, 0, 20, 1, 1 )
+
+
 		#QtGui.QWidget.hide(self.btnMenu)
 
 	def tabla2(self):
-		self.btnAtras = QtGui.QPushButton(self, text = 'Volver')
+		self.btnAtras = QtGui.QPushButton(self)
+		self.btnAtras.setIcon(QtGui.QIcon('back_button.ico'))
 		self.gridLayout2.addWidget(self.btnAtras, 0, 0, 1, 1)
 
-		self.btnAdelante = QtGui.QPushButton(self, text = 'Adelante')
+
+		self.btnAdelante = QtGui.QPushButton(self)
+		self.btnAdelante.setIcon(QtGui.QIcon('next_button.ico'))
 		self.gridLayout2.addWidget(self.btnAdelante, 0, 1, 1, 1)
 
+		self.btnReload = QtGui.QPushButton(self)
+		self.btnReload.setIcon(QtGui.QIcon('reload.ico'))
+		self.gridLayout2.addWidget(self.btnReload, 0, 2, 1, 1)
+
 		self.direcc = QtGui.QLabel(u'Dirección', self)
-		self.gridLayout2.addWidget(self.direcc, 0, 2, 1, 1)
+		self.gridLayout2.addWidget(self.direcc, 0, 3, 1, 1)
 
 		self.urlBox = QtGui.QLineEdit(self)
-		self.gridLayout2.addWidget(self.urlBox, 0, 3, 1, 1)
+		self.gridLayout2.addWidget(self.urlBox, 0, 4, 1, 1)
 
 	def tabla3(self):
 		#El navegador en sí
@@ -87,18 +108,20 @@ class VentanaNav(QtGui.QWidget):
 		self.urlBox.setText(str(_url))
 		self.navegadorP.load(QtCore.QUrl(_url))
 		
-	'''
-		self.navegadorV = QtWebKit.QWebView(self)
-		self.gridLayout.addWidget(self.navegadorV, 2, 4, 1, 5)
+	'''	
+		self.navegador2 = QtWebKit.QWebView(self)
+		self.gridLayout3.addWidget(self.navegador2, 0, 1, 1, 1)
+		self.navegador2.load(QtCore.QUrl(_url))
 
-		_url = "http://www.google.es"
-		self.navegadorV.load(QtCore.QUrl(_url))
+		self.navegador3 = QtWebKit.QWebView(self)
+		self.gridLayout3.addWidget(self.navegador3, 1, 0, 1, 1)
+		self.navegador3.load(QtCore.QUrl(_url))
 
-		#Se crea una 'segunda pantalla' que por defecto está oculta. *Tengo que implementar un menu con la opcion de mostrarla.
-		self.navegadorH = QtWebKit.QWebView(self)
-		self.gridLayout.addWidget(self.navegadorH, 4, 0, 1, 9)
+		self.navegador4 = QtWebKit.QWebView(self)
+		self.gridLayout3.addWidget(self.navegador4, 1, 1, 1, 1)
+		self.navegador4.load(QtCore.QUrl(_url))
 
-		# QtGui.QWidget.hide(self.navegador2) # esto oculta navegadorH
+		# QtGui.QWidget.hide(self.navegadorV) # esto oculta navegadorV
 
 	#De momento lo voy a ocultar, porque no se que sera mas eficiente, si ir creando y destruyendo o tan solo ocultando.
 	'''
@@ -113,26 +136,74 @@ class VentanaNav(QtGui.QWidget):
 			QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
 
 	def acciones(self):
+		''' #faltan cosas por pulir
 		QtCore.QObject.connect(
-			self.urlBox,
-			QtCore.SIGNAL('returnPressed()'),
-			self.cargarUrl)
+			self.gridLayout1,
+			QtCore.SIGNAL('clicked()'),
+			self.moverVentana
+			)
+		'''
+		#tabla1
+		QtCore.QObject.connect(
+			self.btnQuitar,
+			QtCore.SIGNAL('clicked()'),
+			exit
+			)
+		QtCore.QObject.connect(
+			self.btnMax,
+			QtCore.SIGNAL('clicked()'),
+			self.maximizar
+			)
+		QtCore.QObject.connect(
+			self.btnMin,
+			QtCore.SIGNAL('clicked()'),
+			self.minimizar
+			)
 
+		#tabla 2
 		QtCore.QObject.connect(
 			self.btnAdelante,
 			QtCore.SIGNAL('clicked()'),
-			self.navegadorP.forward)
+			self.navegadorP.forward
+			)
 
 		QtCore.QObject.connect(
 			self.btnAtras,
 			QtCore.SIGNAL('clicked()'),
-			self.navegadorP.back)
+			self.navegadorP.back
+			)
 
+		QtCore.QObject.connect(
+			self.btnReload,
+			QtCore.SIGNAL('clicked()'),
+			self.navegadorP.reload
+			)
+
+		QtCore.QObject.connect(
+			self.urlBox,
+			QtCore.SIGNAL('returnPressed()'),
+			self.cargarUrl
+			)
+
+		#tabla3
 		self.navegadorP.connect(  #de lo que forma parte
 			self.navegadorP,		#el "objeto" que se ve implicado
 			QtCore.SIGNAL('urlChanged(QUrl)'),	#la senyal que recibe
 			self.actualizarUrl		#lo que ocurre al recibir la senyal
 			)
+	''' faltan cosas por pulir
+	def moverVentana(self):
+		self.maximizar()
+		self.move(QtGui.QMouseEvent.globalPos())
+	'''
+	def maximizar (self):
+		if self.isMaximized() == False:
+			self.showMaximized()
+		else:
+			self.showNormal()
+
+	def minimizar (self):
+			self.showMinimized()
 
 	def actualizarUrl(self):
 		self.urlBox.setText(str(self.navegadorP.url().toString()))
